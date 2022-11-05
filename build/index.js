65d5e264905bd25947a0d88997d63402d30f0c7e
adevcarol <alvscarolina@outlook.com>
2022-11-05 00:31:53 -0300

    Estudo de arrays, funções e interfaces

--- build/index.js
+++ build/index.js
@@ -1,8 +1,83 @@
 "use strict";
-function printaObjeto(pessoa) {
-    console.log(pessoa);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// tipos primitivos: boolean, number, string
+let ligado = false; // o :boolean serve para travar o tipo da variável e ela não poderá receber um tipo que são seja do mesmo tipo do declarado
+let nome = "carol";
+let idade = 28;
+// tipos especiais: null, undefined
+let nulo = null;
+let indefinido = undefined;
+// tipos abrangentes: any, void
+let retorno;
+let retornoView = false;
+// objeto - sem presivibilidade
+let produto = {
+    name: "carol",
+    cidade: "bm",
+    idade: 30,
+};
+let meuProduto = {
+    nome: "tenis",
+    preco: 125.90,
+    unidades: 12,
+};
+/*
+ * arrays
+ */
+let dados = ["carol", "ana", "maria"]; // ou
+let dados2 = ["carol", "ana", "maria"];
+// array de multitipos
+let infos = ["carol", 28]; // não é muito usual
+/*
+ * tuplas
+ */
+let boleto = ["agua conta", 199.9, 252557657]; // é possível determinar a posição dos tipos diferentemente dos arrays de multitipos
+/*
+ * métodos de array
+ */
+dados.pop();
+/*
+ * datas
+ */
+let aniversario = new Date("2022-12-01 05:00");
+console.log(aniversario.toDateString());
+/*
+ * funções
+ */
+function addNumber(x, y) {
+    return x + y;
 }
-printaObjeto({
-    name: "bruce wayne",
-    vulgo: "batman",
-});
+function addToHello(name) {
+    return `Hello ${name}`;
+}
+// funções podem aceitar mais de um parametro como tipo
+function callToPhone(phone) {
+    return phone;
+}
+function getDatabase(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return "carol";
+    });
+}
+let soma = addNumber(4, 7);
+let nameFunction = addToHello("CAROL");
+console.log(soma);
+console.log(nameFunction);
+const bot1 = {
+    id: 1,
+    name: "megaman",
+};
+const bot2 = {
+    id: 1,
+    name: "megaman",
+};
+console.log(bot1);
+console.log(bot2);
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,104 @@ let indefinido:undefined = undefined;
 // tipos abrangentes: any, void
 let retorno:void;
 let retornoView:any = false;
+
+// objeto - sem presivibilidade
+let produto:object = {
+    name: "carol",
+    cidade: "bm",
+    idade: 30,
+}
+
+type ProdutoLoja = {
+    nome: string;
+    preco: number;
+    unidades: number;
+}
+
+let meuProduto: ProdutoLoja = {
+    nome: "tenis",
+    preco: 125.90,
+    unidades: 12,
+}
+
+/*
+ * arrays
+ */
+
+let dados: string[] = ["carol", "ana", "maria"]; // ou
+let dados2: Array<string> = ["carol", "ana", "maria"];
+
+// array de multitipos
+let infos: (string | number)[] = ["carol", 28]; // não é muito usual
+
+/*
+ * tuplas
+ */
+
+let boleto:[string, number, number] = ["agua conta", 199.9, 252557657] // é possível determinar a posição dos tipos diferentemente dos arrays de multitipos
+
+/*
+ * métodos de array
+ */
+
+dados.pop()
+
+/*
+ * datas
+ */
+
+let aniversario: Date = new Date("2022-12-01 05:00")
+console.log(aniversario.toDateString())
+
+/*
+ * funções
+ */
+
+function addNumber(x: number, y: number): number{
+    return x + y; 
+}
+
+function addToHello(name: string): string{
+    return `Hello ${name}`;
+}
+
+// funções podem aceitar mais de um parametro como tipo
+function callToPhone(phone: number | string): number | string{
+    return phone;
+}
+
+async function getDatabase(id: number): Promise<string>{ // promisse
+    return "carol"
+}
+
+let soma: number = addNumber(4, 7);
+let nameFunction: string = addToHello("CAROL");
+console.log(soma);
+console.log(nameFunction);
+
+/*
+ * interfaces (type x interface)
+ */
+
+type robot = { // para tipar um obj
+    readonly id: number;
+    name: string;
+};
+
+interface robot2 { // para trabalhar com classes
+    id: number | string;
+    name: string;
+}
+
+const bot1: robot2 = {
+    id: 1,
+    name: "megaman",
+};
+
+const bot2: robot2 = {
+    id: 1,
+    name: "megaman",
+};
+
+console.log(bot1);
+console.log(bot2);
